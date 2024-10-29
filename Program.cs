b2b3b44fd94cd982b385120647375a0350ba611c
Luke Bleijenbergh <lukeheeftmail@gmail.com>
2024-10-29 15:17:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace test
+{
+    class Test{ 
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number <= 2)
+            {
+                Console.WriteLine(2);
+            }
+            else
+            {
+                Console.Write(NextPrime(number));
+            }
+
+            
+        }
+        static int NextPrime(int n)
+        {
+            if (IsPrime(n))
+            {
+                return n;
+            }
+            return NextPrime(n + 1);
+        }
+
+        static bool IsPrime(int n)
+        {
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (IsDivisor(n, i)) return false;
+            }
+            return true;
+        }
+
+        static bool IsDivisor(int n, int d)
+        {
+            if (n % d == 0) return true;
+            return false;
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+﻿This project was made during my first year of university.
+
+It is a console application that returns the following prime.
